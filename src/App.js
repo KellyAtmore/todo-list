0297cef13264fcbbfe18783795cbcbfe3413d19d
Kelly Atmore <kelly.atmore47@gmail.com>
2022-08-11 13:51:03 -0700

    get input text value

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
       <header>
         <h1>Kelly's Todo List</h1>
       </header>
       <Form />
+      <TodoList />
     </div>
   );
 }
